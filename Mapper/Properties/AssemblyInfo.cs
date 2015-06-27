3eb8f2fc4c7d7de955c9059251fc796325b96932
Robson Rocha - Treinamento e Consultoria <contato@robsonrocha.com.br>
2015-06-27 03:36:23 -0300

    Adicionado pacote de publicação do NuGet e informações de Assembly atualizadas

--- Mapper/Properties/AssemblyInfo.cs
+++ Mapper/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RobsonROX.Mapper")]
+[assembly: AssemblyDescription("Implementação simplificada porém versátil de um Data Mapper para .NET Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mapper")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Robson Rocha Treinamento e Consultoria")]
+[assembly: AssemblyProduct("RobsonROX.Mapper")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 Robson Rocha de Araújo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
